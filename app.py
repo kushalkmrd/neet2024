20fde35c0cece72e3f07668f0f411dbf37ecfadd
Kushal <89372726+kushalkmrd@users.noreply.github.com>
2024-07-20 06:06:50 -0400

    added requiremetns

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly as px
-
-import streamlit as st
-import pandas as pd
-import plotly as px
+import plotly.express as px
 
 # install plotly express 
 
